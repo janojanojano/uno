80f7c2dd860aaee1c3c3ec13574300a3c18089bd
janojanojano <87879376+janojanojano@users.noreply.github.com>
2021-07-24 22:15:41 +0000

    Update instal2.sh

--- instal2.sh
+++ instal2.sh
@@ -9,6 +9,6 @@ sudo pacman -S xf86-video-ati
 sudo pacman -S xf86-video-intel intel-ucode
 sudo pacman -S xorg-server xorg-xinit mesa mesa-demos
 sudo pacman -S lxde
-sudo pacman -S opera leafpad epdfview firefox firefox-i18n-es-cl file-roller smplayer deluge
+sudo pacman -S opera leafpad epdfview firefox firefox-i18n-es-cl file-roller smplayer deluge gnome-disk-utility
 sudo pacman -S lxdm
 sudo systemctl enable lxdm.servi
